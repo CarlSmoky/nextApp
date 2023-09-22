fe61405dfdce477c543b06105e17fb803079d055
tsumitakaoru <k.tsumita01@gmail.com>
2023-09-22 18:23:07 -0400

    app route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
+import SectionWrapper from './components/SectionWrapper'
 
 export default function Home() {
   return (
-    <div className='text-7xl text-blue-500'>Home</div>
+    <div className='text-7xl text-blue-500'><SectionWrapper/></div>
+    
   )
 }
