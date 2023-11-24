9e8bdf638ccc07de79c7adbae3f38c26318f0ad8
tsumitakaoru <k.tsumita01@gmail.com>
2023-11-24 09:16:19 -0500

    Change font size

--- app/components/ParagraphWrapper.tsx
+++ app/components/ParagraphWrapper.tsx
@@ -7,7 +7,7 @@ interface Props {
 const ParagraphWrapper: React.FC<Props> = ({ paragraph }: Props) => {
   return (
     <div className="w-auto mt-10 md:mx-10 md:mt-0 md:w-2/3">
-        <p className="text-left text-grey-100 text-xl lg:text-2xl font-paragraph">
+        <p className="text-left text-grey-100 text-sm sm:text-base md:text-lg lg:text-xl font-paragraph">
           {paragraph}
         </p>
       </div>
