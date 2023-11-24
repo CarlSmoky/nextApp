0694eefb030ee84b3a7c2ecefd8f10ea09bf7bbd
tsumitakaoru <k.tsumita01@gmail.com>
2023-11-24 15:38:47 -0500

    add hover animation

--- app/components/Footer/FooterNav.tsx
+++ app/components/Footer/FooterNav.tsx
@@ -20,14 +20,14 @@ const FooterNav = () => {
         {links.map((link: Link, i) => (
           <li key={i} className="">
             <Link href={link.link}>
-              <h3 className="py-3 px-0 md:px-1 lg:px-2">{link.name}</h3>
+              <h3 className="text-grey-200 py-3 px-0 md:px-1 lg:px-2 hover:text-black-100 transition-all duration-300 ease-in-out">{link.name}</h3>
             </Link>
             {link.submenu && (
               <div className="flex flex-col text-grey-200/80 text-xs">
                 <ul>
                   {link.sublinks.map((slink, i) => (
                     <div key={i}>
-                      <li className="py-3 px-0 md:px-1 lg:px-2">
+                      <li className="py-3 px-0 md:px-1 lg:px-2 hover:text-black-100 transition-all duration-300 ease-in-out">
                         <Link href={slink.link}>{slink.name}</Link>
                       </li>
                     </div>
