44f6f59e5261fee7618f22f10bf09487f4756a05
tsumitakaoru <k.tsumita01@gmail.com>
2023-12-04 11:14:23 -0500

    Add DisplayNavProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,29 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import type { Metadata } from "next";
 import { childrenProps } from "./types/Interfaces";
-import { orbitron, raleway, sofiaSans, montserrat, nunito ,quicksand, oswald, paragraphFont, titleFont } from "./fonts";
+import { DisplayNavProvider } from "./provider/DisplayNavProvider";
+import {
+  orbitron,
+  raleway,
+  sofiaSans,
+  montserrat,
+  nunito,
+  quicksand,
+  oswald,
+  paragraphFont,
+  titleFont,
+} from "./fonts";
+import React, { useContext, createContext, useState } from "react";
+
+// go into the types:
+enum NavState {
+  home,
+  visualArt,
+  peformance,
+  nia,
+  about,
+  contact
+}
 
 export const metadata: Metadata = {
   title: {
@@ -13,14 +35,21 @@ export const metadata: Metadata = {
   description: "Artist & Dancer Noriko Yamamoto sites",
 };
 
+
 const RootLayout: React.FC<childrenProps> = ({ children }: childrenProps) => {
+
   return (
     <html lang="en">
       <body className="bg-prime-100">
-        <main className={`${titleFont.variable} ${raleway.variable} ${sofiaSans.variable} ${paragraphFont.variable}`}>
-          <Navbar />
-          {children}
-          <Footer />
+        <main
+          className={`${titleFont.variable} ${raleway.variable} ${sofiaSans.variable} ${paragraphFont.variable}`}
+        >
+          <DisplayNavProvider>
+            <Navbar />
+            {children}
+            <Footer />
+          </DisplayNavProvider>
+          
         </main>
       </body>
     </html>
