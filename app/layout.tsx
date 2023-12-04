15f18e2ce3a84d25bbbe2b4812702e287642dc15
tsumitakaoru <k.tsumita01@gmail.com>
2023-12-04 11:17:12 -0500

    Clean code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,11 @@
+import React from "react";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import type { Metadata } from "next";
 import { childrenProps } from "./types/Interfaces";
 import { DisplayNavProvider } from "./provider/DisplayNavProvider";
-import {
-  orbitron,
-  raleway,
-  sofiaSans,
-  montserrat,
-  nunito,
-  quicksand,
-  oswald,
-  paragraphFont,
-  titleFont,
-} from "./fonts";
-import React, { useContext, createContext, useState } from "react";
-
-// go into the types:
-enum NavState {
-  home,
-  visualArt,
-  peformance,
-  nia,
-  about,
-  contact
-}
+import { paragraphFont, titleFont } from "./fonts";
 
 export const metadata: Metadata = {
   title: {
@@ -35,21 +15,16 @@ export const metadata: Metadata = {
   description: "Artist & Dancer Noriko Yamamoto sites",
 };
 
-
 const RootLayout: React.FC<childrenProps> = ({ children }: childrenProps) => {
-
   return (
     <html lang="en">
       <body className="bg-prime-100">
-        <main
-          className={`${titleFont.variable} ${raleway.variable} ${sofiaSans.variable} ${paragraphFont.variable}`}
-        >
+        <main className={`${titleFont.variable} ${paragraphFont.variable}`}>
           <DisplayNavProvider>
             <Navbar />
             {children}
             <Footer />
           </DisplayNavProvider>
-          
         </main>
       </body>
     </html>
