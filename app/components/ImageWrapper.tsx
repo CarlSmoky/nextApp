2c5fd787ab2fbf17543fd19208a20d5c17b42823
tsumitakaoru <k.tsumita01@gmail.com>
2024-01-17 10:43:41 -0500

    Change image size

--- app/components/ImageWrapper.tsx
+++ app/components/ImageWrapper.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const ImageWrapper: React.FC<Props> = ({ src, alt }: Props) => {
   return (
-    <div className="m-auto md:mt-0 md:ml-0 w-2/3 sm:w-1/3 lg:w-auto">
+    <div className="m-auto md:mt-0 md:ml-0 w-2/3 lg:w-auto">
       <Image className="m-auto md:mt-0 md:ml-0 object-cover aspect-square rounded-full md:rounded-none md:shadow-2xl" src={src} alt={alt} height={350} width={350} />
     </div>
   )
