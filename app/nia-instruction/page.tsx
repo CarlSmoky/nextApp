a34e84b56fc327d244300f07562023cab9403d87
tsumitakaoru <k.tsumita01@gmail.com>
2024-01-22 09:02:55 -0500

    Add spaces

--- app/nia-instruction/page.tsx
+++ app/nia-instruction/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: 'Nia instruction',
 }
 
-const paragraph =`Nia blends modern and ethnic dance with elements of martial arts, tai chi, and yoga to give a low impact, full ‘mind, body and spirit’ workout. It’s fun and invigorating. No dance experience necessary. You need only enjoy moving to music......barefooted!!! Noriko has been a certified Nia Instructor since 2000, and since the beginning of the pandemic, she’s shifted her classes to just one online Zoom class on Thursday mornings at 9:15am EST. You're welcome to join any time – just email Noriko for more details.`
+const paragraph =`Nia blends modern and ethnic dance with elements of martial arts, tai chi, and yoga to give a low impact, full ‘mind, body and spirit’ workout. It’s fun and invigorating. No dance experience necessary. You need only enjoy moving to music ..... barefooted!!! Noriko has been a certified Nia Instructor since 2000, and since the beginning of the pandemic, she’s shifted her classes to just one online Zoom class on Thursday mornings at 9:15am EST. You're welcome to join any time – just email Noriko for more details.`
 
 const NiaInstructionPage: React.FC = () => {
   return (
