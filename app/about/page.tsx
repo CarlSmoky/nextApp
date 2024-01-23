fda2cc79e4f0c96f230c491ea29d93f81709aae5
tsumitakaoru <k.tsumita01@gmail.com>
2024-01-23 12:30:35 -0500

    refactor

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,21 +40,17 @@ const AboutPage: React.FC = () => {
           </ItalicWrapper>
           <LongTextWithButtonWrapper>
             <br />
-            <p>
             After a long successful performance career in Tokyo, Noriko Yamamoto moved to Toronto in 1994 to be with her husband. She initially came with two suitcases, her son, cat, and rice cooker. <span className="italic text-sm md:text-base lg:text-lg">(the gallery is named after this cat)</span>
-            </p>
             <br />
-            <p>
+            <br />
             She has continued this career in Toronto with hundreds of performances at venues ranging from schools and libraries to festivals, galleries, and theatres.
-            </p>
             <br />
-            <p>
+            <br />
             Noriko’s visual art life has been greatly influenced by her performance career as a dancer, mime, clown, Silent Storyteller, and choreographer in both Japan and Canada. A great part of her visual art expresses movement, often shown through nature or the human figure. She subtly uses lines, layers, shadows, and colour to create texture-rich sculptures and mixed-media works. She strives to evoke motion and emotion in stillness.
-            </p>
             <br />
-            <p>
+            <br />
             In 2019 at the Promise of Sekishu Washi exhibition at Propeller Art Gallery in Toronto, her piece, Sora, received an Honourable Mention from the judges and won the Audience Choice Award. It was at this point that she joined the collective, Washi Sisters, and since then, has been enjoying the challenge of using traditional Japanese handmade paper in her artworks.
-            </p>
+            <br />
             <br />
           </LongTextWithButtonWrapper>
         </TextWithImageWrapper>
--- app/components/LongTextWithButtonWrapper.tsx
+++ app/components/LongTextWithButtonWrapper.tsx
@@ -3,24 +3,22 @@ import React, { useState } from "react";
 import { childrenProps } from "../types/Interfaces";
 import UnderlineButton from "./UnderlineButton";
 
-const LongTextWithButtonWrapper: React.FC<childrenProps> = ({ children }: childrenProps) => {
+const LongTextWithButtonWrapper: React.FC<childrenProps> = ({
+  children,
+}: childrenProps) => {
   const [open, setOpen] = useState<boolean>(false);
 
   const handleToggle = () => {
     setOpen(!open);
   };
   return (
-    <>
-      <div
-        className={`${
+    <div>
+      <p className={`${
           open ? "line-clamp-none" : "line-clamp-7"
-        } text-left text-grey-100 text-base md:text-lg lg:text-xl font-paragraph tracking-wide`}
-      >
-        {children}
-      </div>
+        } text-left text-grey-100 text-base md:text-lg lg:text-xl font-paragraph tracking-wide`}>{children}</p>
       <UnderlineButton onClick={handleToggle} open={open} />
-      </>
-  )
-}
+    </div>
+  );
+};
 
-export default LongTextWithButtonWrapper
+export default LongTextWithButtonWrapper;
