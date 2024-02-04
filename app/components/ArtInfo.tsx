680a0b6e2f26d7acb1e90aee7e11e17cb22bac2a
tsumitakaoru <k.tsumita01@gmail.com>
2024-02-04 16:28:14 -0500

    Change display text

--- app/components/ArtInfo.tsx
+++ app/components/ArtInfo.tsx
@@ -3,25 +3,32 @@ import React, { PropsWithChildren } from "react";
 interface Props {
   alt: string;
   size: string;
-  price: number;
+  media: string;
+  year: number;
 }
 
-const ArtInfo: React.FC<Props> = ({ alt, size, price }: Props) => {
+const ArtInfo: React.FC<Props> = ({ alt, size, media, year }: Props) => {
   return (
     <div className="w-auto mt-4 mb-10 md:mt-0 md:ml-8 md:w-1/2 text-left text-grey-100  tracking-wide">
       <h3 className="font-title font-bold text-lg md:text-xl lg:text-2xl">
         {alt}
       </h3>
+      {media && (
+        <p>
+          <span className="text-sm">Media: </span>
+          {media}
+        </p>
+      )}
       {size && (
         <p>
           <span className="text-sm">Size: </span>
           {size}
         </p>
       )}
-      {(price || price > 0) && (
+      {year && (
         <p>
-          <span className="text-sm">Price: CAD$</span>
-          {price}
+          <span className="text-sm">Year: </span>
+          {year}
         </p>
       )}
     </div>
