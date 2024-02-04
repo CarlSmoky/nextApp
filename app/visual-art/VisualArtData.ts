2a39b95b366c580bbf1cb246c52fb6e4cb59ffb7
tsumitakaoru <k.tsumita01@gmail.com>
2024-02-04 17:49:59 -0500

    Change data

--- app/visual-art/VisualArtData.ts
+++ app/visual-art/VisualArtData.ts
@@ -3,13 +3,13 @@ import { VisualArtType } from "../types/Interfaces";
 export const visualArtData = [
   {
     id: 1,
-    title: "Fading_memories",   
+    title: "fading_memories",   
     category: VisualArtType.Washi,
     descriptions: "",
     images: [
       {
         id: 1,
-        src: '/images/fading_memories.jpg',
+        src: "/images/fading_memories.jpg",
         alt: "fading_memories",
         size: "",
         media:"",
@@ -18,49 +18,81 @@ export const visualArtData = [
     ]
   },
   {
-    id: 1,
+    id: 2,
     title: "shadow_impression",   
     category: VisualArtType.Washi,
     descriptions: "",
     images: [
       {
         id: 1,
-        src: '/images/shadow_impression_01.jpg',
-        alt: "shadow_impression_01",
+        src: "/images/shadow_impression_01.jpg",
+        alt: "shadow_impression_#1",
         size: "14.5 in x 10 in",
         media:"Seichosen Kozo Yotsuban washi, water colour, konnyaku powder, copper wire",
         year: 2023,
       },
       {
-        id: 1,
-        src: '/images/shadow_impression_02.jpg',
-        alt: "shadow_impression_02",
+        id: 2,
+        src: "/images/shadow_impression_02.jpg",
+        alt: "shadow_impression_#2",
         size: "14.5 in x 10 in",
         media:"Seichosen Kozo Yotsuban washi, water colour, konnyaku powder, copper wire",
         year: 2023,
       },
       {
-        id: 1,
-        src: '/images/shadow_impression_03.jpg',
-        alt: "shadow_impression_03",
+        id: 3,
+        src: "/images/shadow_impression_03.jpg",
+        alt: "shadow_impression_#3",
         size: "14.5 in x 10 in",
         media:"Seichosen Kozo Yotsuban washi, water colour, konnyaku powder, copper wire",
         year: 2023,
       },
       {
-        id: 1,
-        src: '/images/shadow_impression_04.jpg',
-        alt: "shadow_impression_04",
+        id: 4,
+        src: "/images/shadow_impression_04.jpg",
+        alt: "shadow_impression_#4",
         size: "14.5 in x 10 in",
         media:"Seichosen Kozo Yotsuban washi, water colour, konnyaku powder, copper wire",
         year: 2023,
       },
       {
-        id: 1,
-        src: '/images/shadow_impression_05.jpg',
-        alt: "shadow_impression_05",
+        id: 5,
+        src: "/images/shadow_impression_05.jpg",
+        alt: "shadow_impression_#5",
         size: "14.5 in x 10 in",
         media:"Seichosen Kozo Yotsuban washi, water colour, konnyaku powder, copper wire",
+        year: 2023,
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "whispering_lights",   
+    category: VisualArtType.Washi,
+    descriptions: "",
+    images: [
+      {
+        id: 1,
+        src: "/images/whispering_lights_01.jpg",
+        alt: "whispering_lights_#1",
+        size: "",
+        media:"",
+        year: 2023,
+      },
+      {
+        id: 2,
+        src: "/images/whispering_lights_02.jpg",
+        alt: "whispering_lights_#2",
+        size: "",
+        media:"",
+        year: 2023,
+      },
+      {
+        id: 3,
+        src: "/images/whispering_lights_03.jpg",
+        alt: "whispering_lights_#3",
+        size: "",
+        media:"",
         year: 2023,
       }
     ]
