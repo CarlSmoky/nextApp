6d91e98c2ca0941a95c88a9d802175993180e1d3
tsumitakaoru <k.tsumita01@gmail.com>
2024-02-06 10:53:44 -0500

    Add Shadow and animation

--- app/components/Gallery/ImgContainerWithStaticImage.tsx
+++ app/components/Gallery/ImgContainerWithStaticImage.tsx
@@ -13,10 +13,10 @@ type Props = {
 
 const ImgContainer = ({ image, numberOfImage, title, i }: Props) => {
   return (
-    <div className="w-full md:w-[calc(50%-8px)] lg:w-[calc(33.3%-8px)] bg-white-200/40 aspect-square relative my-1 md:m-1">
-      <div className="w-[99%] h-[99%] m-auto">
+    <div className="w-full md:w-[calc(50%-16px)] lg:w-[calc(33.3%-16px)] bg-white-200/40 aspect-square my-2 md:m-2 shadow-xl active:ring active:ring-white-100 relative">
+      <div className="w-[97%] h-[97%] m-auto">
         <Link href={`/visual-art/${title.toLowerCase()}`} target="_blank">
-        <div className="w-[96%] h-[96%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="w-[93%] h-[93%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hover:scale-105 transition duration-300 ease-in-out">
             {numberOfImage > 1 && (
               <span className="absolute right-0 p-1 text-white-100 font-title text-sm bg-grey-200/30 rounded-xl z-10">
                 1/{numberOfImage}
