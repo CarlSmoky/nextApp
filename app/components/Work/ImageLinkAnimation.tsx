ac7d2176cb171c9beded254b5275f14c7d7b0d09
tsumitakaoru <k.tsumita01@gmail.com>
2024-02-13 15:35:15 -0500

    Add actice for image links

--- app/components/Work/ImageLinkAnimation.tsx
+++ app/components/Work/ImageLinkAnimation.tsx
@@ -28,7 +28,7 @@ const ImageLinkAnimation: React.FC<Props> = ({
       onClick={() => navContext?.setCurrentNav(navState)}
       className="w-full"
     >
-      <div className="flex from-teal-100 via-teal-300 to-teal-500 bg-gradient-to-br shadow-2xl cursor-pointer">
+      <div className="flex from-teal-100 via-teal-300 to-teal-500 bg-gradient-to-br shadow-2xl active:ring active:ring-white-100 cursor-pointer">
         <div className="relative w-full aspect-video sm:aspect-square overflow-hidden group">
           <div className="absolute z-10 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition duration-300 ease-in-out from-black-100/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white-100 flex items-end">
             <div className="transform-gpu p-4 space-y-3 text-xl lg:group-hover:translate-y-0 translate-y-4 pb-10 transition duration-300 ease-in-out">
