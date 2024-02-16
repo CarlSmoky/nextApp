7ba1a3caf0d63b06ded1976bda3686554639ed42
tsumitakaoru <k.tsumita01@gmail.com>
2024-02-16 14:54:55 -0500

    Change footer style

--- app/components/Footer/FooterNav.tsx
+++ app/components/Footer/FooterNav.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -9,28 +9,36 @@ import { removeExcessivePathSegments } from "../../utils/textFormat";
 const FooterNav = () => {
   const pathname = usePathname();
   return (
-      <ul className="flex flex-col lg:flex-row px-4 md:px-0 font-paragraph text-sm whitespace-nowrap">
-        {links.map((link: LinkInfo, i) => (
-          <li key={i} className="">
-            <Link href={link.link}>
-              <h3 className={`text-grey-200 m-2 py-3 px-1 text-sm md:text-base lg:text-sm xl:text-base hover:text-black-100 transition-all duration-300 ease-in-out ${removeExcessivePathSegments(pathname) ===  link.link ? 'underline underline-offset-4' : ""}`}>{link.name}</h3>
-            </Link>
-            {link.submenu && (
-              <div className="flex flex-col text-grey-200/80 text-xs">
-                <ul>
-                  {link.sublinks.map((slink: Sublink, i) => (
-                    <div key={i}>
-                      <li className="py-3 px-0 md:px-1 lg:px-2 hover:text-black-100 transition-all duration-300 ease-in-out">
-                        <Link href={slink.link}>{slink.name}</Link>
-                      </li>
-                    </div>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </li>
-        ))}
-      </ul>
+    <ul className="w-full xl:w-2/3 flex flex-col lg:flex-row px-4 md:px-0 font-paragraph whitespace-nowrap">
+      {links.map((link: LinkInfo, i) => (
+        <li key={i} className="lg:w-1/6">
+          <Link href={link.link}>
+            <h3
+              className={`text-grey-200 m-2 py-2 px-1 text-sm md:text-base lg:text-sm xl:text-base hover:text-black-100 transition-all duration-300 ease-in-out ${
+                removeExcessivePathSegments(pathname) === link.link
+                  ? "underline underline-offset-4"
+                  : ""
+              }`}
+            >
+              {link.name}
+            </h3>
+          </Link>
+          {link.submenu && (
+            <div className="flex flex-col text-grey-200/80 text-xs">
+              <ul>
+                {link.sublinks.map((slink: Sublink, i) => (
+                  <Link key={i} href={slink.link}>
+                    <li className="ml-3 p-2 hover:text-black-100 transition-all duration-300 ease-in-out">
+                      {slink.name}
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
+          )}
+        </li>
+      ))}
+    </ul>
   );
 };
 
