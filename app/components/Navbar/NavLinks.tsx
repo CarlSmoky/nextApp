6324b180c1834d512b5f1b030c9aa049c88a3fc0
tsumitakaoru <k.tsumita01@gmail.com>
2024-02-17 14:12:27 -0500

    Change Link position

--- app/components/Navbar/NavLinks.tsx
+++ app/components/Navbar/NavLinks.tsx
@@ -7,38 +7,47 @@ import { links } from "./links";
 import { RiArrowUpSLine, RiArrowDownSLine } from "react-icons/ri";
 import { removeExcessivePathSegments } from "../../utils/textFormat";
 
-
-const NavLinks: React.FC<{ onToggle: () => void }> = ({ onToggle })  => {
+const NavLinks: React.FC<{ onToggle: () => void }> = ({ onToggle }) => {
   const [heading, setHeading] = useState<string>("");
   const pathname = usePathname();
 
   const clickHandler = () => {
     onToggle();
     setHeading("");
-  }
-  
+  };
+
   return (
     <>
       {links.map((link: LinkInfo, i) => (
         <div key={i}>
           <div className="text-left md:cursor-pointer group">
             <h1
-              className={`py-7 px-3 lg:px-2 flex justify-between align-baseline text-grey-200 text-base xl:text-lg font-paragraph hover:text-black-100 ${removeExcessivePathSegments(pathname) ===  link.link ? 'underline underline-offset-4' : ""}`}
+              className={`py-7 px-3 lg:px-2 flex align-baseline text-grey-200 text-base xl:text-lg font-paragraph hover:text-black-100 ${
+                removeExcessivePathSegments(pathname) === link.link ? "underline underline-offset-4" : ""}`}
             >
-              <Link onClick={clickHandler} href={link.link}>{link.name}</Link>
-              {link.submenu && (<span className="text-lg lg:text-sm lg:hidden inline" onClick={() => {
-                heading !== link.name ? setHeading(link.name) : setHeading("");
-              }}>
-                {heading === link.name ? (
-                  <RiArrowUpSLine />
-                ) : (
-                  <RiArrowDownSLine />
-                )}
-              </span>)}
+              <Link onClick={clickHandler} href={link.link}>
+                {link.name}
+              </Link>
               {link.submenu && (
-              <div className="hidden text-lg lg:flex lg:justify-center lg:my-auto group-hover:text-black-100 group-hover:rotate-180 transition-all duration-600 ease-in-out">
-                <RiArrowDownSLine />
-              </div>
+                <span
+                  className="text-2xl mx-4 px-2 lg:hidden inline"
+                  onClick={() => {
+                    heading !== link.name
+                      ? setHeading(link.name)
+                      : setHeading("");
+                  }}
+                >
+                  {heading === link.name ? (
+                    <RiArrowUpSLine />
+                  ) : (
+                    <RiArrowDownSLine />
+                  )}
+                </span>
+              )}
+              {link.submenu && (
+                <div className="hidden text-lg lg:flex lg:justify-center lg:my-auto lg:px-1 group-hover:text-black-100 group-hover:rotate-180 transition-all duration-600 ease-in-out">
+                  <RiArrowDownSLine />
+                </div>
               )}
             </h1>
             {link.submenu && (
@@ -46,16 +55,15 @@ const NavLinks: React.FC<{ onToggle: () => void }> = ({ onToggle })  => {
                 <div className="absolute top-20 hidden group-hover:lg:block hover:lg:block z-10">
                   <div className="bg-prime-100 rounded-sm grid grid-cols-3 px-5 ">
                     {link.sublinks.map((slink: Sublink, i) => (
-                      <div key={i}>
+                      <Link
+                        key={i}
+                        href={slink.link}
+                        className="hover:text-black-100 transition-all duration-300 ease-in-out"
+                      >
                         <li className="text-sm font-paragraph text-grey-200 my-2.5">
-                          <Link
-                            href={slink.link}
-                            className="hover:text-black-100 transition-all duration-300 ease-in-out"
-                          >
-                            {slink.name}
-                          </Link>
+                          {slink.name}
                         </li>
-                      </div>
+                      </Link>
                     ))}
                   </div>
                 </div>
@@ -70,13 +78,11 @@ const NavLinks: React.FC<{ onToggle: () => void }> = ({ onToggle })  => {
           >
             {/* sublinks */}
             {link.sublinks.map((slinks: Sublink, i) => (
-              <div key={i}>
-                <div>
-                  <li className="py-3 pl-14 text-grey-200 text-sm font-paragraph hover:text-black-100 transition-all duration-300 ease-in-out">
-                    <Link onClick={clickHandler} href={slinks.link}>{slinks.name}</Link>
-                  </li>
-                </div>
-              </div>
+              <Link key={i} onClick={clickHandler} href={slinks.link}>
+                <li className="py-3 pl-14 text-grey-200 text-sm font-paragraph hover:text-black-100 transition-all duration-300 ease-in-out">
+                  {slinks.name}
+                </li>
+              </Link>
             ))}
           </div>
         </div>
