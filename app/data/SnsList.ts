988773c383d75ba2cc49b99a5e2c80f8ee2fd12e
tsumitakaoru <k.tsumita01@gmail.com>
2024-02-27 15:06:02 -0500

    Change folder name

---
+++ app/data/SnsList.ts
@@ -0,0 +1,11 @@
+
+export const SnsList = [
+  {
+    href: "https://www.instagram.com/noriko_toronto/",
+    iconName: "instagram",
+  },
+  {
+    href: "https://www.youtube.com/@norikoyamamoto7147",
+    iconName: "youtube",
+  },
+];
