0d79e096640c8ee14ba5e8021324c2c0b6af62ef
tsumitakaoru <k.tsumita01@gmail.com>
2024-02-27 10:44:13 -0500

    Use ul,li tag

--- app/components/Gallery/ImgContainerWithStaticImage.tsx
+++ app/components/Gallery/ImgContainerWithStaticImage.tsx
@@ -1,18 +1,17 @@
-import React from "react";
 import Image from "next/image";
 import { VisualArtImage } from "../../types/Interfaces";
 import Link from "next/link";
 
-type Props = {
+type ImgContainerProps = {
   image: VisualArtImage;
   numberOfImage: number;
   title: string;
   i: number;
 };
 
-const ImgContainer = ({ image, numberOfImage, title, i }: Props) => {
+const ImgContainer:React.FC<ImgContainerProps> = ({ image, numberOfImage, title, i }) => {
   return (
-    <div className="w-full md:w-[calc(50%-16px)] lg:w-[calc(33.3%-16px)] bg-white-200/40 aspect-square my-2 md:m-2 shadow-xl active:ring active:ring-white-100 relative">
+    <li className="w-full md:w-[calc(50%-16px)] lg:w-[calc(33.3%-16px)] bg-white-200/40 aspect-square my-2 md:m-2 shadow-xl active:ring active:ring-white-100 relative">
       <div className="w-[97%] h-[97%] m-auto">
         <Link href={`/visual-art/${title.toLowerCase()}`}>
         <div className="w-[93%] h-[93%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:hover:scale-105 transition duration-300 ease-in-out">
@@ -32,7 +31,7 @@ const ImgContainer = ({ image, numberOfImage, title, i }: Props) => {
           </div>
         </Link>
       </div>
-    </div>
+    </li>
   );
 };
 
