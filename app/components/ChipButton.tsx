dba3e3df9303541556ad336ef1d579daa330dc5e
tsumitakaoru <k.tsumita01@gmail.com>
2024-03-04 11:38:14 -0500

    Refactor

--- app/components/ChipButton.tsx
+++ app/components/ChipButton.tsx
@@ -1,36 +1,28 @@
-'use client'
-import { useRouter } from 'next/navigation'
-import { IoCheckmark } from "react-icons/io5";
 import { toTitleCase } from "../utils/textFormat";
-import { makePath } from "../utils/datahandling";
 import { PerformanceType } from "../types/Interfaces";
 
 
 type ChipButtonProps = {
-  params: any;
-  types: PerformanceType[];
-  type: PerformanceType;
-  path: any;
+  type?: PerformanceType;
+  text?: string;
+  isSelected?: boolean;
+  selectedAll?: boolean;
+  clickHandler: () => void;
 }
 
-const ChipButton:React.FC<ChipButtonProps> = ({params, types, type , path}) => {
-  const router = useRouter();
-  const clickHandler = (): void => {
-    const updatedPath = makePath(params, types, type, path)
-    router.push(updatedPath)
-    router.refresh();
-  }
+const ChipButton:React.FC<ChipButtonProps> = ({type, text, isSelected, selectedAll = false, clickHandler}) => {
+  
   return (
     <button
-          key={type}
           onClick={clickHandler}
           className={`flex justify-center rounded-full border border-grey-100/20 p-[0.5rem] min-w-[5rem] whitespace-nowrap md:hover:bg-grey-100/80 md:hover:text-white-100 transition ease-in-out 
-          ${(params.length === 0 || params.includes(type)) && "bg-grey-100/30 "}
+          ${isSelected && "bg-grey-100/30 "}
+          ${selectedAll && "outline outline-offset-2 outline-2 outline-grey-100/50"}
           `}
+          disabled={selectedAll}
         >
-          {(params.length === 0 || params.includes(type)) 
-            && <IoCheckmark className='my-auto mr-1'/>}
-          {toTitleCase(type)}
+
+          {type ? toTitleCase(type) : text}
         </button>
   )
 }
