267d72aa468c358fa1090c77d7b871f74100469c
tsumitakaoru <k.tsumita01@gmail.com>
2024-03-12 11:55:49 -0400

    Add blur

--- app/components/Gallery/GalleryWithStaticImage.tsx
+++ app/components/Gallery/GalleryWithStaticImage.tsx
@@ -1,16 +1,30 @@
-import ImgContainerWithStaticImage from "./ImgContainerWithStaticImage"
-import { visualArtData } from '../../visual-art/VisualArtData'
+import ImgContainerWithStaticImage from "./ImgContainerWithStaticImage";
+import { visualArtData } from "../../visual-art/VisualArtData";
+import { VisualArtSeries } from "../../types/Interfaces";
+import { getBase64 } from "../../utils/getBase64";
 
-const GalleryWithStaticImage = async () => {
-  if (!visualArtData) return <h2 className="m-4 text-sxl font-bold">No images Found</h2>
+const GalleryWithStaticImage: React.FC = async () => {
+  if (!visualArtData)
+    return <h2 className="m-4 text-sxl font-bold">No images Found</h2>;
+
+  let data: VisualArtSeries[]  = [...visualArtData];
+  for (let i = 0; i < data.length; i++) {
+    const base64 = await getBase64(data[i].images[0].src);
+    data[i].images[0].blurredDataUrl = base64;
+  }
 
   return (
-   <ul className="flex flex-col md:flex-row flex-wrap">
-      {visualArtData.map((series, i) => (
-        <ImgContainerWithStaticImage image={series.images[0]} numberOfImage={series.images.length} title={series.title} key={i} i={i}/>
+    <ul className="flex flex-col md:flex-row flex-wrap">
+      {data.map((series) => (
+          <ImgContainerWithStaticImage
+            image={series.images[0]}
+            numberOfImage={series.images.length}
+            title={series.title}
+            key={series.title}
+        />
       ))}
-   </ul>
-  )
-}
+    </ul>
+  );
+};
 
-export default GalleryWithStaticImage
+export default GalleryWithStaticImage;
