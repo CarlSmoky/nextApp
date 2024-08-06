a60d3e68e879a6bb3988f7c7ff2672ae7b0e52c1
tsumitakaoru <k.tsumita01@gmail.com>
2024-08-06 17:43:08 -0400

    Improve accessibility

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 const spaces = {
   long: `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0`,
   betweenWords: `\xa0\xa0\xa0\xa0`,
-}
+};
 
 const AboutPage: React.FC = () => {
   return (
@@ -19,29 +19,45 @@ const AboutPage: React.FC = () => {
       <div className="flex-wrapper">
         <PortraitImage src="/images/noriko.jpg" alt="portrait" />
         <TextInfoWrapper breakePointWidth="twoThirds">
-          <p className="italic text-sm md:text-base lg:text-lg pt-6 md:pt-0">
+          <p className="italic text-sm md:text-base lg:text-lg py-6 md:pt-0" > 
             {spaces.long}loves...
-            <br />
-            cats{spaces.betweenWords}cello{spaces.betweenWords}skywatching{spaces.betweenWords}crescent moons
-            <br />
+            <br aria-hidden={true}/>
+            cats{spaces.betweenWords}cello{spaces.betweenWords}skywatching
+            {spaces.betweenWords}crescent moons
+            <br aria-hidden={true}/>
             Charlie Chaplin{spaces.betweenWords}Jean-Louis Barrault
-            <br />
+            <br aria-hidden={true}/>
             Alberto Giacometti{spaces.betweenWords}Isamu Noguchi
-          </p>
+          </p >
           <TextWithlineClampButton>
-            <br />
-            After a long successful performance career in Tokyo, Noriko Yamamoto moved to Toronto in 1994 to be with her husband. She initially came with two suitcases, her son, cat, and rice cooker. <span className="italic text-sm md:text-base lg:text-lg">(the gallery is named after this special cat)</span>
-            <br />
-            <br />
-            She has continued this career in Toronto with hundreds of performances at venues ranging from schools and libraries to festivals, galleries, and theatres.
-            <br />
-            <br />
-            Noriko’s visual art life has been greatly influenced by her performance career as a dancer, mime, clown, Silent Storyteller, and choreographer in both Japan and Canada. A great part of her visual art expresses movement, often shown through nature or the human figure. She subtly uses lines, layers, shadows, and colour to create texture-rich sculptures and mixed-media works. She strives to evoke motion and emotion in stillness.
-            <br />
-            <br />
-            In 2019 at the Promise of Sekishu Washi exhibition at Propeller Art Gallery in Toronto, her piece, Sora, received an Honourable Mention from the judges and won the Audience Choice Award. It was at this point that she joined the collective, Washi Sisters, and since then, has been enjoying the challenge of using traditional Japanese handmade paper in her artworks.
-            <br />
-            <br />
+              After a long successful performance career in Tokyo, Noriko
+              Yamamoto moved to Toronto in 1994 to be with her husband. She
+              initially came with two suitcases, her son, cat, and rice cooker.{" "}
+              <span className="italic text-sm md:text-base lg:text-lg">
+                (the gallery is named after this special cat)
+              </span>
+              <br aria-hidden={true}/>
+              <br aria-hidden={true}/>
+              She has continued this career in Toronto with hundreds of
+              performances at venues ranging from schools and libraries to
+              festivals, galleries, and theatres.
+              <br aria-hidden={true}/>
+              <br aria-hidden={true}/>
+              Noriko’s visual art life has been greatly influenced by her
+              performance career as a dancer, mime, clown, Silent Storyteller,
+              and choreographer in both Japan and Canada. A great part of her
+              visual art expresses movement, often shown through nature or the
+              human figure. She subtly uses lines, layers, shadows, and colour
+              to create texture-rich sculptures and mixed-media works. She
+              strives to evoke motion and emotion in stillness.
+              <br aria-hidden={true}/>
+              <br aria-hidden={true}/>
+              In 2019 at the Promise of Sekishu Washi exhibition at Propeller
+              Art Gallery in Toronto, her piece, Sora, received an Honourable
+              Mention from the judges and won the Audience Choice Award. It was
+              at this point that she joined the collective, Washi Sisters, and
+              since then, has been enjoying the challenge of using traditional
+              Japanese handmade paper in her artworks.
           </TextWithlineClampButton>
         </TextInfoWrapper>
       </div>
--- app/components/TextInfoWrapper.tsx
+++ app/components/TextInfoWrapper.tsx
@@ -7,9 +7,9 @@ type ComplimentProps = {
 
 const TextInfoWrapper: React.FC<ComplimentProps> = ({ children, breakePointWidth }) => {
   return (
-    <div className={`w-full ${breakePointWidth === "half" ? "md:w-1/2" : "md:w-2/3"} mt-4 md:mt-0 md:ml-8 text-left text-grey-100 text-base md:text-lg lg:text-xl font-paragraph tracking-wide`}>
+    <article className={`w-full ${breakePointWidth === "half" ? "md:w-1/2" : "md:w-2/3"} mt-4 md:mt-0 md:ml-8 text-left text-grey-100 text-base md:text-lg lg:text-xl font-paragraph tracking-wide`}>
       {children}
-    </div>
+    </article>
   )
 }
 
